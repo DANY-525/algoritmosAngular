28e3adc4375b8f1b1687f8b138dd24da49ecb506
daniel <dany525@gmail.com>
2021-05-14 16:17:31 -0500

    primer algoritmo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algoritmos';
+  matriz: any = [1, 2, 3,4,4, 4, 5];
+  obj: any = { datos: [] }
+  mayor: number = 0;
+  ngOnInit() {
+    for (let index = 0; index < this.matriz.length; index++) {
+      const element = this.matriz[index];
+      this.getVeces(element);
+    }
+
+    for (let i = 0; i < this.obj['datos'].length; i++) {
+      let variable = this.obj['datos'][i]['longest']
+      if (variable > this.mayor) {
+        this.mayor = this.obj['datos'][i]['longest'];
+      }
+    }
+    let numerico = this.obj['datos'].filter((a: { longest: number; }) => a.longest === this.mayor)
+    console.log("Number " + numerico[0]['number']);
+    console.log("Longest " + numerico[0]['longest']);
+  }
+  getVeces(number: number) {
+    const result = this.matriz.filter((word: number) => word == number);
+    this.obj['datos'].push({ "number": number, "longest": result.length })
+  }
+
 }
